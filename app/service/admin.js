4fe57c4c61006ee6781471afc9563d99069eb6c0
liduv5 <570577701@qq.com>
2019-05-21 01:49:02 +0800

    权限V3

--- app/service/admin.js
+++ app/service/admin.js
@@ -15,7 +15,7 @@ class AdminService extends Service {
     let userinfo = this.ctx.session.userinfo
     let role_id = userinfo.role_id
     let pathname = url.parse(this.ctx.request.url).pathname    // 获取当前访问的地址
-    let ignoreUrl = [ '/login', '/menu']
+    let ignoreUrl = ['/login', '/menu']
     if (ignoreUrl.includes(pathname) || userinfo.is_super === 1) {
       return true
     }
@@ -23,13 +23,13 @@ class AdminService extends Service {
     let accessResult = await this.ctx.model.RoleAccess.find({ "role_id": role_id })
     let accessArray = []
     accessResult.forEach((value) => {
-      accessArray.push(JSON.stringify(value.access_id))
+      accessArray.push(value.access_id.toString())
     })
     /* 3、获取当前访问的url对应的权限id */
     let accessUrlResult = await this.ctx.model.Access.find({ url: pathname })
     /* 4、判断当前访问的url对应的权限id 是否在权限列表中 */
     if (accessUrlResult.length > 0) {
-      if (accessArray.includes(JSON.stringify(accessUrlResult[0]._id))) {
+      if (accessArray.includes(accessUrlResult[0]._id.toString())) {
         return true
       }
       return false
